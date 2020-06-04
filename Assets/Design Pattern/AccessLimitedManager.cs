0df1efa557d9dfc0f538bce95bce01938853ffef
gyd <kingterrygyd@gmail.com>
2020-06-04 14:06:17 +0800

    Add Design Pattern and First Sample
    Add:
    1. Add Design Pattern Folder
    2. Add Design Pattern: AccessLimitedManager

---
+++ Assets/Design Pattern/AccessLimitedManager.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * design pattern sample:AccessLimited class and Manager
+ * Date 20200603
+ * Author: Chun-Lung Tseng as Gyd
+ * Email: kingterrygyd@gmail.com
+ * Twitter: @kingterrygyd
+ * Facebook: facebook.com/barbariangyd
+ */
+
+using UnityEngine;
+
+public class AccessLimitedManager : AccessLimited.Manager { }
+
+public class AccessLimited
+{
+    private int _limitedIntData = 0;
+    private AccessLimited()
+    {
+
+    }
+
+    public class Manager : MonoBehaviour
+    {
+        public AccessLimited Create()
+        {
+            AccessLimited accessLimited = new AccessLimited();
+            return accessLimited;
+        }
+
+        public void SetIntData(AccessLimited accessLimited, int value)
+        {
+            accessLimited._limitedIntData = value;
+        }
+    }
+}
